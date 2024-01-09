e12f29920cc91edbe825ac98dc4c6dffee9e3071
Aleksei Baiukov <Blue51@yandex.ru>
2024-01-09 02:54:52 +0100

    initial commit

---
+++ js/map/map.config.js
@@ -0,0 +1,15 @@
+export const mapConfig = {
+	api: "pk.eyJ1IjoiZGlvZzBvIiwiYSI6ImNscXZibm5mYTNrcG0yanA5emd1ZG1sdngifQ.4PH3ctj8ywj6QArcHYbOyg",
+	defaultCity: "Prague, Czech Rebulic",
+	defaultPlace: {
+		name: "Prague, Czechia",
+		coords: {
+			lat: 50.080345,
+			lng: 14.428973,
+		}
+	},
+	routingProfile: "driving",
+	userName: "diog0o",
+	styleID: "clqvd51k600yt01o9eafbhhfn",
+	zoom: 8
+}
